2aacc13eb03ede7d11dc03577c4ebe7b6ae5b108
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-01-12 10:54:58 -0300

    dynamic routing and db connection

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import "dotenv/config"
 import  express  from "express";
 import  cors  from 'cors'
-
+import { router } from './routes'
+import db from "../config/mongo"
 const PORT = process.env.PORT || 3001
 const app = express()
 app.use(cors({
     origin:['http://localhost:3000']
 }))
-
+app.use(router)
+db().then(() => console.log("Connected to db"))
 app.listen(PORT, () => console.log("Server Online on port " + `${PORT}`))
